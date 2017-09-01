9f7b5323371a900ff809fe998b1e4b4d5e0bb910
omar navarro <omm27@hotmail.com>
2017-09-01 00:08:45 -0500

    se hicieron los metodos completos ya que cada metodo en particular de solo una cancion es un metodo auxiliar para los metodos mas grandes. osea, que un metodo borrartodo tiene un sub metodo borrar uno.

--- Biblioteca.java
+++ Biblioteca.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 public class Biblioteca
 {
     private ArrayList<String> canciones;
-    private Reproductor reproductor; 
+    private Reproductor reproductor;
+    
     
     /**
      * Se inicializa la lista de objetos de Archivos o canciones
@@ -13,6 +14,7 @@ public class Biblioteca
     {
         canciones = new ArrayList<String>();
         reproductor= new Reproductor();
+        
     }
     
     /**
@@ -25,6 +27,51 @@ public class Biblioteca
     
     public void reproducirCan(int numeroCan)
     {
-        reproductor.reproducir(canciones.get(numeroCan));
+        int existe;
+        existe=totalpistas();
+        
+        if(existe!=0)
+        {
+            reproductor.reproducir(canciones.get(numeroCan));
+        }
+        
+    }
+    
+    public void eliminar(int numeroCan)
+    {
+        int existe;
+        existe=totalpistas();
+        
+        if(existe!=0)
+        {
+            canciones.remove(numeroCan);
+        }
+    }
+    
+    public int totalpistas()
+    {
+        int i=canciones.size();
+        return i;
+    }
+    
+    public void reproducirTodas()
+    {
+        int existe;
+        existe=totalpistas();
+        for(int i=0;i<existe;i++)
+        {
+            reproducirCan(i);
+        }
+    }
+    
+    public void eliminartodo()
+    {
+        int existe;
+        existe=totalpistas();
+        
+        for(int i=0;i<existe;i++)
+        {
+            eliminar(i);
+        }
     }
 }
